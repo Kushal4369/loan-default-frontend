2f1fc8961f2445352d46066e08bd94877c359d5c
Kushal4369 <143159808+Kushal4369@users.noreply.github.com>
2025-11-13 16:19:21 +0530

    Deployed on Cloud

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function MLInputPage() {
     setResult(null);
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/predict", {
+      const res = await fetch("https://loan-default-backend.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
